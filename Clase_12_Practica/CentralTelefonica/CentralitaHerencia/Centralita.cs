2492720a72bc3f0c6b0125b59f59ee75b211e174
alumno <alumno@utnfra.com.ar>
2019-09-25 12:03:35 -0300

    Centralita

---
+++ Clase_12_Practica/CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Centralita
+    {
+        #region Atributos
+        private List<Llamada> _listaDeLlamadas;
+        protected string _razonSocial;
+        #endregion
+
+
+
+
+    }
+}
---
+++ Clase_12_Practica/CentralTelefonica/CentralitaHerencia/Enumerados.cs
@@ -0,0 +1,13 @@
+﻿public enum Franja
+{
+    Franja_1,
+    Franja_2,
+    Franja_3
+}
+
+public enum TipoLlamada
+{
+    Local,
+    Provincial,
+    Todas
+}
