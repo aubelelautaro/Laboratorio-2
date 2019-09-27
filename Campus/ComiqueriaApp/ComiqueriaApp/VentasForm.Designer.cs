b77dc574fe15b0bc98574d22cd08b58cb5b5a25c
Gilberto <calderongilberto3@gmail.com>
2019-09-26 22:49:04 -0300

    Campus

---
+++ Campus/ComiqueriaApp/ComiqueriaApp/VentasForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ComiqueriaApp
+{
+    partial class VentasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownCantidad = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecioFinal = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCantidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownCantidad
+            // 
+            this.numericUpDownCantidad.Location = new System.Drawing.Point(42, 122);
+            this.numericUpDownCantidad.Name = "numericUpDownCantidad";
+            this.numericUpDownCantidad.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownCantidad.TabIndex = 0;
+            this.numericUpDownCantidad.ValueChanged += new System.EventHandler(this.NumericUpDownCantidad_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cantidad:";
+            // 
+            // btnVender
+            // 
+            this.btnVender.Location = new System.Drawing.Point(42, 193);
+            this.btnVender.Name = "btnVender";
+            this.btnVender.Size = new System.Drawing.Size(103, 23);
+            this.btnVender.TabIndex = 2;
+            this.btnVender.Text = "Vender";
+            this.btnVender.UseVisualStyleBackColor = true;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 25);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(31, 13);
+            this.lblDescripcion.TabIndex = 4;
+            this.lblDescripcion.Text = "hola";
+            // 
+            // lblPrecioFinal
+            // 
+            this.lblPrecioFinal.AutoSize = true;
+            this.lblPrecioFinal.Location = new System.Drawing.Point(200, 122);
+            this.lblPrecioFinal.Name = "lblPrecioFinal";
+            this.lblPrecioFinal.Size = new System.Drawing.Size(101, 13);
+            this.lblPrecioFinal.TabIndex = 5;
+            this.lblPrecioFinal.Text = "Precio Final:  $X.XX";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(203, 193);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(103, 23);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // VentasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 277);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lblPrecioFinal);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.btnVender);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownCantidad);
+            this.Name = "VentasForm";
+            this.Text = "Nueva Venta";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCantidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVender;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecioFinal;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
---
+++ Campus/ComiqueriaApp/ComiqueriaApp/VentasForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComiqueriaLogic;
+
+namespace ComiqueriaApp
+{
+    public partial class VentasForm : Form
+    {
+        private Producto producto;
+        private Venta venta;
+
+        public VentasForm()
+        {
+            InitializeComponent();
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            this.numericUpDownCantidad.Minimum = 1;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.DialogResult = DialogResult.Yes;
+
+        }
+
+        public VentasForm(Producto producto):this()
+        {
+            this.producto = producto;
+            this.lblPrecioFinal.Text = this.lblPrecioFinal.Text.Replace("X.XX",this.producto.Precio.ToString());
+            this.lblDescripcion.Text = producto.Descripcion;
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NumericUpDownCantidad_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
