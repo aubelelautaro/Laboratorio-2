637de897cf27807bafc295fac7df362f419d03b9
alumno <alumno@utnfra.com.ar>
2019-10-16 12:06:29 -0300

    Clases_Apuntes y Ejercicio_48

---
+++ Clases_Apuntes/Depositos_Prog_II/Entidades/DepositoDeCocinas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DepositoDeCocinas
+    {
+        #region Atributos
+        private int _capacidadMaxima;
+        private List<Cocina> _lista;
+        #endregion
+
+        #region Constructores
+        public DepositoDeCocinas(int capacidad)
+        {
+            this._capacidadMaxima = capacidad;
+            this._lista = new List<Cocina>();
+        }
+        #endregion
+
+
+        #region Metodos
+        private int GetIndice(Cocina c)
+        {
+            for (int i = 0; i < this._lista.Count; i++)
+            {
+                if (this._lista[i].Equals(c))
+                    return i;
+            }
+            return -1;
+        }
+
+        public bool Agregar(Cocina c)
+        {
+            return this + c;
+        }
+
+        public bool Remover(Cocina c)
+        {
+            return this - c;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Capacidad Maxima: {this._capacidadMaxima}");
+            sb.AppendLine("Listado de Cocinas:");
+            foreach (Cocina value in this._lista)
+            {
+                sb.AppendLine(value.ToString());
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+
+        #region Sobrecarga de Operadores
+        public static bool operator +(DepositoDeCocinas d, Cocina c)
+        {
+            if (d._capacidadMaxima > d._lista.Count)
+            {
+                d._lista.Add(c);
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator -(DepositoDeCocinas d, Cocina c)
+        {
+            int indice = d.GetIndice(c);
+
+            if (indice != -1)
+            {
+                d._lista.RemoveAt(indice);
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Guia/Ejercicio_48/Contabilidad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_48
+{
+    public class Contabilidad<T,U> where T:  Documento where U: Documento, new()
+    {
+        #region Atributos
+        private List<T> ingresos;
+        private List<U> egresos;
+        #endregion
+
+        public Contabilidad()
+        {
+            this.egresos = new List<U>();
+            this.ingresos = new List<T>();
+        }
+
+        #region Metodos
+        public static Contabilidad<T,U> operator +(Contabilidad<T, U> c, T ingreso)
+        {
+         
+
+                return c;
+        }
+        #endregion
+    }
+}
