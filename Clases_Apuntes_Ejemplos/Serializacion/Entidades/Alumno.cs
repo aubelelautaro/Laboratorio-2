bafc7e78d064675015c0d8e4bc1f273e4624ce85
Gilberto Calderón <47418056+gilbertocp@users.noreply.github.com>
2019-10-29 12:03:31 -0300

    Add files via upload

---
+++ Clases_Apuntes_Ejemplos/Serializacion/Entidades/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno:Persona
+    {
+        public double nota;
+
+        public Alumno(string nombre, string apellido, int edad, double nota):base(nombre, apellido, edad)
+        {
+            this.nota = nota;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Nota: {this.nota}";
+        }
+    }
+}
---
+++ Clases_Apuntes_Ejemplos/Serializacion/Entidades/Auto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization; // debe estar incluida para serializar 
+using System.Xml;
+using System.IO;
+
+namespace Entidades
+{
+    public class Auto:IXML
+    {
+        public string marca;
+        public double precio;
+
+        public Auto(string marca, double precio)
+        {
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public Auto() { }
+
+        public override string ToString()
+        {
+            return $"Marca: {this.marca}, Precio: {this.precio}";
+        }
+
+        public bool Guardar(string str)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Auto));
+                using (XmlTextWriter xmlTextWriter = new XmlTextWriter(str, Encoding.UTF8))
+                {
+                    xmlSerializer.Serialize(xmlTextWriter, this);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Leer(string str, out object obj)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Persona));
+                using (XmlTextReader xmlTextReader = new XmlTextReader(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\persona.xml"))
+                {
+                    obj = (Persona)xmlSerializer.Deserialize(xmlTextReader);
+                }
+              
+            }
+            catch
+            {
+                obj = null;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
